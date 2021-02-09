7b7dfaaf4423c38c340b1b95939f525a8ee2109c
Ben <viirens@gmail.com>
2021-02-09 10:02:29 +0100

    Oh

--- perlin1d.js
+++ perlin1d.js
@@ -8,23 +8,28 @@ function setup() {
 function draw() {
     
     background(0);
-    stroke(255);
-    noFill();
+    stroke(0, 255, 0);
+    
     beginShape();
     var xoff = start;
     for (var x = 0; x < width; x++) {
-        stroke(255);
-        var y = noise(xoff) * height;
-     
-        vertex(x, y);
 
+        stroke(0, 255, 0);
+        var y = noise(xoff) * height;
+        vertex(x, y);
+        fill(0, 255, 0);
         xoff += inc;
-        
+
     }
+    start += inc;
+    vertex(width, height);
+  　vertex(0, height);
     endShape();
     start += inc;
 
-
+    console.log(y);
+    /* 
+    lerpColor((0, 0, 0), (0, 255, 0), .5) */
 
 
 
